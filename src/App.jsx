2fd8b962b9287bc6a33fe20a10c833d3b0d05bc9
JKBoy2244 <jobayer2006@gmail.com>
2026-04-01 21:56:42 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Clock from "./clock";
 import "./index.css";
+import sea from './assets/images/sea.png';
+
 
 export default function App() {
+  return <img src={sea} alt="Sea" />;
   return <Clock intro="Hello there!" name="Jobayer" age={20} university="Queen Mary University of London" course="Computer Systems Engineering" number={5} />;;
 }
