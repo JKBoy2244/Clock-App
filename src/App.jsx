dcf9424b1c865780295ec982d88452d884f7b282
JKBoy2244 <jobayer2006@gmail.com>
2026-04-01 22:12:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Clock from "./clock";
 import "./index.css";
-import sky from "./assets/images/sky.png";
+import mosque from "./assets/images/mosque.png";
 
 export default function App() {
   return (
     <div
       className="app"
-      style={{ backgroundImage: `url(${sea})` }}
+      style={{ backgroundImage: `url(${mosque})` }}
     >
       <Clock intro="Hello there!" name="Jobayer" age={20} university="Queen Mary University of London" course="Computer Systems Engineering" number={5}
       />
