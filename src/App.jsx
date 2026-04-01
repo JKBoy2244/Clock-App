01f543d3df8c4afb3031a4c400417fca2415e06e
JKBoy2244 <jobayer2006@gmail.com>
2026-04-01 22:02:52 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import Clock from "./clock";
 import "./index.css";
-
+import sea from "./assets/images/sea.png";
 
 export default function App() {
-  return <img src={sea} alt="Sea" />;
-  return <Clock intro="Hello there!" name="Jobayer" age={20} university="Queen Mary University of London" course="Computer Systems Engineering" number={5} />;;
+  return (
+    <div
+      className="app"
+      style={{ backgroundImage: `url(${sea})` }}
+    >
+      <Clock intro="Hello there!" name="Jobayer" age={20} university="Queen Mary University of London" course="Computer Systems Engineering" number={5}
+      />
+    </div>
+  );
 }
